847e1869fcbd565331ae438b9845bcd0cd64af3e
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2023-05-25 15:23:13 +0530

    Moving to community.lua
    Using community maintained plugins insted of using from user.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -16,4 +16,31 @@ return {
   { import = "astrocommunity.editing-support.auto-save-nvim" },
   { import = "astrocommunity.editing-support.nvim-regexplainer" },
   { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  {
+    "folke/todo-comments.nvim",
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    },
+  },
+  { import = "astrocommunity.code-runner.overseer-nvim" },
+  {
+    "stevearc/overseer.nvim",
+    keys = {
+      { "<leader>Oo", "<cmd>OverseerOpen<cr>",         desc = "Overseer Open" },
+      { "<leader>Oc", "<cmd>OverseerClose<cr>",        desc = "Overseer Close" },
+      { "<leader>Ot", "<cmd>OverseerToggle<cr>",       desc = "Overseer Toggle" },
+      { "<leader>Os", "<cmd>OverseerSaveBundle<cr>",   desc = "Overseer Bundle" },
+      { "<leader>Ol", "<cmd>OverseerLoadBundle<cr>",   desc = "Overseer Load Bundle" },
+      { "<leader>Od", "<cmd>OverseerDeleteBundle<cr>", desc = "Overseer Delete Bundle" },
+      { "<leader>OR", "<cmd>OverseerRunCmd<cr>",       desc = "Overseer Run Cmd" },
+      { "<leader>Or", "<cmd>OverseerRun<cr>",          desc = "Overseer Run" },
+      { "<leader>Oi", "<cmd>OverseerInfo<cr>",         desc = "Overseer Info" },
+      { "<leader>Ob", "<cmd>OverseerBuild<cr>",        desc = "Overseer Build" },
+      { "<leader>Oq", "<cmd>OverseerQuickAction<cr>",  desc = "Overseer Quick Action" },
+      { "<leader>OT", "<cmd>OverseerTaskAction<cr>",   desc = "Overseer Task Action" },
+      { "<leader>OC", "<cmd>OverseerClearCache<cr>",   desc = "Overseer Clear Cache" },
+      { "<Leader>O",  desc = "Overseer",               name = "Overseer" },
+    },
+  },
 }
